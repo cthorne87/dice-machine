0db1b228c6aacef8d84e5f48497b28ed143df0c6
Chris Thorne <u1400900@umail.utah.edu>
2022-03-10 18:58:51 -0700

    layout

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,12 @@ let dice = $(":button").on("click", function(event){
     //console.log(multiplier);
     var sum = 0;
     for (let i = 0; i < multiplier; i++){
-        result = Math.floor(Math.random() * event.target.id + 1);
-        sum += result;
+        roll = Math.floor(Math.random() * event.target.id + 1);
+        sum += roll;
         //console.log(sum);
-    }
-    console.log(sum);
+    };
+    //console.log(sum);
+    document.getElementById("results").innerHTML = sum
     
-})
+});
 
